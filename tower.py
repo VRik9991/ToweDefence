179ee36cb09d9b34027cac03f65b34a86810ed44
VRik <vrik9449@gmail.com>
2025-03-12 21:23:17 +0300

    I did 72/81 of task

---
+++ tower.py
@@ -0,0 +1,25 @@
+import pygame
+import math
+from utils import DamageType
+
+
+class Tower:
+    def __init__(self, attack_speed: float, damage_type: DamageType, demage: int):
+        self.attack_speed = attack_speed
+        self.damage_type: DamageType = damage_type
+        self.attack_damage = demage
+        self.rect = None
+        self.image = pygame.image.load("—Pngtree—stereo 2.5d building tower defense_4055247.png")
+        self.range = 40
+        self.cooldown = 0.2
+        self.projectiles = []
+
+    def display(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def place(self, x, y):
+        self.rect = pygame.rect(x, y, 10, 10)
+    def attack(self,monsters):
+        for monster in monsters:
+
+            if monster.rect.center.x
---
+++ utils.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class DamageType(Enum):
+    EARTH = 'earth'
+    WATER = 'water'
+    AIR = 'air'
+    FIRE = 'fire'
