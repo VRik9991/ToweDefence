6f5e5bbee696d53b9dcfced63b3e6908a8c217f1
alexe <alexey.perepel@gmail.com>
2025-03-14 22:34:20 +0300

    Map

---
+++ map.py
@@ -0,0 +1,5 @@
+class BaseTower:
+    def __init__(self, attack_speed, attack_damage, damage_types, rect, image, cooldown, projectiles):
+        self.rect = pygame.Rect(x, y, side1, side2)
+        self.speed_x = speed_x
+        self.color = (0, 153, 0)
