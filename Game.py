27c63b5e80cf98bc2e1a9d42388f9be5e3532870
VRik <vrik9449@gmail.com>
2025-03-19 21:31:55 +0300

    Around half done

---
+++ Game.py
@@ -0,0 +1,21 @@
+
+
+class Game:
+    def __init__(self,monsters,towers,map):
+        self.monsters=monsters
+        self.towers=towers
+        self.map = map
+        self.path = self.map.route
+    def display(self):
+        for monster in self.monsters:
+            monster.display()
+        for tower in self.towers:
+            tower.display()
+        self.map.display()
+    def run(self):
+        for tower in self.towers:
+            tower.attack()
+        for monster in self.monsters:
+            monster.move()
+
+
