cb8b1c3a90593cf68a1027e89163ad2565b9fed7
VRik <vrik9449@gmail.com>
2025-04-02 19:16:18 +0300

    Fix Tower numer 1

--- towers/Tower.py
+++ towers/Tower.py
@@ -1,30 +1,41 @@
 import pygame
 import math
 from utils import DamageType
-
+from towers.Projectile import *
+import time
 
 class Tower:
     def __init__(self, attack_speed: float, damage_type: DamageType, demage: int):
-        self.attack_speed = attack_speed
         self.damage_type: DamageType = damage_type
         self.attack_damage = demage
         self.rect = pygame.Rect(100,250,10,10)
         self.image = pygame.image.load("towers/assets/tower_assets/small_tower.png")
-        self.range = 40
+        self.range = 500
         self.cooldown = 0.2
         self.projectiles = []
+        self.curect_time = time.time()
 
     def display(self, screen):
         screen.blit(self.image, self.rect)
+        for projectile in self.projectiles:
+            projectile.display(screen)
 
     def place(self, x, y):
         self.rect = pygame.rect(x, y, 10, 10)
 
-    def attack(self, monsters):
-        count = []
-        for monster in monsters:
-            count.append(math.sqrt(
-                (monster.rect.center[0] - self.rect.center[0]) ** 2 + (monster.rect.center[1] - self.rect.center[1]) ** 2))
-        count_smallest = count.index(min(count))
-        self.projectiles.append(monsters[count_smallest])
-        return monsters[count_smallest]
+    def attack(self, monsters: list):
+        if time.time() >= self.curect_time+self.cooldown:
+            count = []
+            for monster in monsters:
+                count.append(math.sqrt(
+                    (monster.rect.center[0] - self.rect.center[0]) ** 2 + (monster.rect.center[1] - self.rect.center[1]) ** 2))
+            if count:
+                nearest_position = count.index(min(count))
+                # nearest_monster = min(monsters, key=lambda monster: (monster.rect.center[0] - self.rect.center[0]) ** 2 + (monster.rect.center[1] - self.rect.center[1]) ** 2)))
+
+                if nearest_position <= self.range:
+                    self.projectiles.append(Projectile(*self.rect.center, monsters[nearest_position],10,50))
+                self.projectiles = [projectile for projectile in self.projectiles if not projectile.hit]
+            for projectile in self.projectiles:
+                projectile.move()
+            self.curect_time = time.time()
