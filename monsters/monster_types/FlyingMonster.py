e537e381860a785242992a0f54d4b1106b02883a
python <petarfilipov37@gmail.com>
2025-04-04 18:32:14 +0200

    cleanup

--- monsters/monster_types/FlyingMonster.py
+++ monsters/monster_types/FlyingMonster.py
@@ -8,7 +8,6 @@ class FlyingMonster(Monster):
     def __init__(self, spawn):
         super().__init__(spawn)
         self.speed = 5
-        self.is_alive = True
         self.resistances = {
             DamageType.EARTH: 0,
             DamageType.WATER: 0,
@@ -17,9 +16,7 @@ class FlyingMonster(Monster):
         }
 
         self.image = pygame.image.load('monsters/assets/flying.png')
-        self.rect = self.image.get_rect(center=spawn)
         self.hp = 6
-        self.counter = 0
 
     def move(self, coordinates):
         dx = coordinates[0] - self.rect.x
@@ -36,11 +33,5 @@ class FlyingMonster(Monster):
             self.rect.y = coordinates[1]-50
             self.counter += 1
 
-        if self.counter == 6:
-            self.is_alive = False
-
         if self.hp <= 0:
             self.is_alive = False
-
-    def display(self, screen):
-        screen.blit(self.image, self.rect)
